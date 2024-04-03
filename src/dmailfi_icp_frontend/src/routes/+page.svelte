701fbfd06d65fb30ffe037fa62180211ff8187a0
wirapratamaz <89260515+wirapratamaz@users.noreply.github.com>
2024-04-03 09:39:12 +0800

    Migrate from dmailfi_web to dmailfii_protocol for Frontend

--- src/dmailfi_icp_frontend/src/routes/+page.svelte
+++ src/dmailfi_icp_frontend/src/routes/+page.svelte
@@ -1,26 +1,157 @@
-<script>
-  import "../index.scss";
-  import { backend } from "$lib/canisters";
+<!-- Hero Section -->
+<div class="relative overflow-hidden bg-gray-50">
+    <div class="hidden sm:absolute sm:inset-y-0 sm:block sm:h-full sm:w-full" aria-hidden="true">
+      <div class="relative mx-auto h-full max-w-7xl">
+        <svg class="absolute right-full translate-x-1/4 translate-y-1/4 transform lg:translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">
+          <defs>
+            <pattern id="4522f7d5-8e8c-43ee-89bd-ad34cbfb07fa" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="784" fill="url(#4522f7d5-8e8c-43ee-89bd-ad34cbfb07fa)" />
+        </svg>
+        <svg class="absolute left-full -translate-x-1/4 -translate-y-3/4 transform md:-translate-y-1/2 lg:-translate-x-1/2" width="404" height="784" fill="none" viewBox="0 0 404 784">
+          <defs>
+            <pattern id="5d0dd344-b041-4d26-bec4-8d33ea57ec9b" x="0" y="0" width="20" height="20" patternUnits="userSpaceOnUse">
+              <rect x="0" y="0" width="4" height="4" class="text-gray-200" fill="currentColor" />
+            </pattern>
+          </defs>
+          <rect width="404" height="784" fill="url(#5d0dd344-b041-4d26-bec4-8d33ea57ec9b)" />
+        </svg>
+      </div>
+    </div>
+  
+    <div class="relative pb-16 pt-6 sm:pb-24">
+      <div>
+        <div class="mx-auto max-w-7xl px-4 sm:px-6">
+          <nav class="relative flex items-center justify-between sm:h-10 md:justify-center" aria-label="Global">
+            <div class="flex flex-1 items-center md:absolute md:inset-y-0 md:left-0">
+              <div class="flex w-full items-center justify-between md:w-auto">
+                <a href="#">
+                  <span class="sr-only">Your Company</span>
+                  <img class="h-8 w-auto sm:h-10" src="https://tailwindui.com/img/logos/mark.svg?color=green&shade=600" alt="">
+                </a>
+                <div class="-mr-2 flex items-center md:hidden">
+                  <button type="button" class="relative inline-flex items-center justify-center rounded-md bg-gray-50 p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-green-500" aria-expanded="false">
+                    <span class="absolute -inset-0.5"></span>
+                    <span class="sr-only">Open main menu</span>
+                    <svg class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" aria-hidden="true">
+                      <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            </div>
+            <!-- <div class="hidden md:flex md:space-x-10">
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Product</a>
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Features</a>
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Marketplace</a>
+              <a href="#" class="font-medium text-gray-500 hover:text-gray-900">Company</a>
+            </div> -->
+            <!-- <div class="hidden md:absolute md:inset-y-0 md:right-0 md:flex md:items-center md:justify-end">
+              <span class="inline-flex rounded-md shadow">
+                <a href="#" class="inline-flex items-center rounded-md border border-transparent bg-white px-4 py-2 text-base font-medium text-green-600 hover:bg-gray-50">Log in</a>
+              </span>
+            </div> -->
+          </nav>
+        </div>
+  
+        <!--
+          Mobile menu, show/hide based on menu open state.
+  
+          Entering: "duration-150 ease-out"
+            From: "opacity-0 scale-95"
+            To: "opacity-100 scale-100"
+          Leaving: "duration-100 ease-in"
+            From: "opacity-100 scale-100"
+            To: "opacity-0 scale-95"
+        -->
+       
+      </div>
+  
+      <main class="mx-auto mt-16 max-w-7xl px-4 sm:mt-24">
+        <div class="text-center">
+          <h1 class="text-4xl font-bold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
+            <span class="block xl:inline">Decentralized</span>
+            <span class="block text-green-600 xl:inline">Mail Finance</span>
+            <span class="block xl:inline">Protocol</span>
+          </h1>
+          <p class="mx-auto mt-3 max-w-md text-base text-gray-500 sm:text-lg md:mt-5 md:max-w-3xl md:text-xl">The Decentralized Mail Finance Protocol empowers businesses to offer mailing services, monetize through newsletter subscriptions, and leverage automated mailing assistants, unlocking new revenue streams from email marketing.</p>
+          <div class="mx-auto mt-5 max-w-md sm:flex sm:justify-center md:mt-8">
+            <div class="rounded-md shadow">
+              <a href="/whitepaper" class="flex w-full items-center justify-center rounded-md border border-transparent bg-green-600 px-8 py-3 text-base font-medium text-white hover:bg-green-700 md:px-10 md:py-4 md:text-lg">Here Our Whitepaper</a>
+            </div>
+          </div>
+        </div>
+      </main>
+    </div>
+  </div>
+  
+  <!-- Features Section -->
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl text-center sm:py-16 lg:px-6">
+        <h2 class="mb-4 text-4xl tracking-tight font-extrabold text-gray-600 dark:text-white">The First Web3 Email Protocol on ICP Blockchain</h2>
+        <p class="text-gray-500 sm:text-xl dark:text-gray-400">What DmailFi means to your Business</p>
+        <div class="mt-8 lg:mt-12 space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0 text-green-600">
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 mx-auto">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+                  </svg>
+                  
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Mailing Services Platform</h3>
+                <p class="mb-4 text-gray-500 dark:text-gray-400">A comprehensive suite for managing and deploying mailing services, designed for businesses of all sizes.</p>
+                <!-- <a href="#" class="inline-flex items-center text-sm font-medium text-green-600 hover:text-green-700 dark:text-green-500 dark:hover:text-green-400">
+                    Learn how to keep your funds safe <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                </a> -->
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 mx-auto">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+                  </svg>
+                  
+                  
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Automated Mailing Assistant</h3>
+                <p class="mb-4 text-gray-500 dark:text-gray-400">It can analyze subscriber behavior to tailor messages, improve open rates, and enhance user engagement.</p>
+                <!-- <a href="#" class="inline-flex items-center text-sm font-medium text-green-600 hover:text-green-700 dark:text-green-500 dark:hover:text-green-400">
+                    Learn how your crypto is covered <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                </a> -->
+            </div>
+            <div>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-12 h-12 mx-auto">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 3v11.25A2.25 2.25 0 0 0 6 16.5h2.25M3.75 3h-1.5m1.5 0h16.5m0 0h1.5m-1.5 0v11.25A2.25 2.25 0 0 1 18 16.5h-2.25m-7.5 0h7.5m-7.5 0-1 3m8.5-3 1 3m0 0 .5 1.5m-.5-1.5h-9.5m0 0-.5 1.5m.75-9 3-3 2.148 2.148A12.061 12.061 0 0 1 16.5 7.605" />
+                  </svg>
+                  
+                <h3 class="mb-2 text-xl font-bold dark:text-white">Newsletter Subscription Monetization</h3>
+                <p class="mb-4 text-gray-500 dark:text-gray-400">Tools to easily set up and manage paid newsletter subscriptions. Includes features for businesses monetize their newsletters effectively.</p>
+                <!-- <a href="#" class="inline-flex items-center text-sm font-medium text-green-600 hover:text-green-700 dark:text-green-500 dark:hover:text-green-400">
+                    How to implement best practices <svg class="ml-1 w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                </a> -->
+            </div>
+        </div>
+    </div>
+  </section>
 
-  let greeting = "";
-
-  function onSubmit(event) {
-    const name = event.target.name.value;
-    backend.greet(name).then((response) => {
-      greeting = response;
-    });
-    return false;
-  }
-</script>
-
-<main>
-  <img src="/logo2.svg" alt="DFINITY logo" />
-  <br />
-  <br />
-  <form action="#" on:submit|preventDefault={onSubmit}>
-    <label for="name">Enter your name: &nbsp;</label>
-    <input id="name" alt="Name" type="text" />
-    <button type="submit">Click Me!</button>
-  </form>
-  <section id="greeting">{greeting}</section>
-</main>
+  <!-- Newsletter -->
+  <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-md sm:text-center">
+            <h2 class="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">Sign up for our newsletter</h2>
+            <p class="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">Stay up to date with the roadmap progress, announcements feel free to sign up with your email.</p>
+            <form action="#">
+                <div class="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
+                    <div class="relative w-full">
+                        <label for="email" class="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
+                        <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
+                            <svg class="w-5 h-5 text-gray-500 dark:text-gray-400" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
+                        </div>
+                        <input class="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500" placeholder="Enter your email" type="email" id="email" required>
+                    </div>
+                    <div>
+                        <button type="submit" class="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-green-700 border-green-600 sm:rounded-none sm:rounded-r-lg hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">Subscribe</button>
+                    </div>
+                </div>
+                <div class="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">We care about the protection of your data. <a href="#" class="font-medium text-green-600 dark:text-green-500 hover:underline">Read our Privacy Policy</a>.</div>
+            </form>
+        </div>
+    </div>
+  </section>
---
+++ src/dmailfi_icp_frontend/src/routes/whitepaper/+page.svelte
@@ -0,0 +1,198 @@
+<script>
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		const links = Array.from(document.querySelectorAll('aside a'));
+
+		links.forEach((link) => {
+			link.addEventListener('click', (event) => {
+				event.preventDefault();
+
+				// @ts-ignore
+				const targetId = event.target.getAttribute('href');
+				const targetSection = document.querySelector(targetId);
+
+				targetSection.scrollIntoView({ behavior: 'smooth' });
+			});
+		});
+	});
+</script>
+
+<div class="container mx-auto px-4 py-8">
+	<nav class="mb-8">
+		<a href="/" class="text-green-400 hover:text-blue-300">Home</a>
+	</nav>
+	<header class="mb-12">
+		<h1 class="text-6xl font-bold text-6xl font-bold text-green-600">DmailFi Whitepaper</h1>
+	</header>
+	<main class="grid grid-cols-3 gap-8">
+		<div class="col-span-2">
+			<section id="abstract" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">Abstract</h2>
+				<p class="text-lg mb- 6 text-justify text-justify">
+					In today's digital age, where communication and finance are pivotal to both personal and
+					professional spheres, DmailFi emerges as a groundbreaking solution, merging email services
+					with decentralized finance (DeFi) functionalities. Utilizing the robustness of blockchain
+					technology, specifically the Internet Computer Protocol (ICP), DmailFi offers a secure,
+					private, and efficient platform for conducting email communication intertwined with
+					financial transactions. This white paper delves into existing challenges within email
+					services and DeFi platforms, unveiling the innovative technology behind DmailFi, its
+					unique features, practical applications, and a forward-looking roadmap.
+				</p>
+			</section>
+			<section id="problem" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">The Problem</h2>
+				<p class="mb- 6 text-justify">
+					DmailFi addresses several critical issues plaguing traditional email services and DeFi
+					platforms:
+				</p>
+				<ul class="list-disc ml-5 mb- 6 text-justify">
+					<li>
+						<strong class="text-green-500">Security Vulnerabilities:</strong> Traditional email services are susceptible to security
+						breaches, such as Business Email Compromise (BEC), and lack native end-to-end encryption
+						for emails at rest.
+					</li>
+					<li>
+						<strong class="text-green-500">High Subscription Costs:</strong> Services like Google Workspace and Outlook impose significant
+						subscription fees, particularly burdensome for organizations with a large number of
+						employees.
+					</li>
+					<li>
+						<strong class="text-green-500">Email Address Generation:</strong> There's a pressing need for generating a multitude of email
+						addresses assignable to customers for auditing purposes, without requiring customers to
+						forfeit their personal email addresses.
+					</li>
+					<li>
+						<strong class="text-green-500">DeFi Integration:</strong> DeFi platforms often present user-unfriendly interfaces and exist
+						separately from everyday communication tools, leading to inefficiencies and heightened
+						security risks.
+					</li>
+				</ul>
+			</section>
+			<section id="solution" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">The Solution</h2>
+				<p class="mb- 6 text-justify">DmailFi innovatively addresses these challenges</p>
+				<ul class="list-disc ml-5 mb- 6 text-justify">
+					<li>
+						<strong class="text-green-500">Cryptographic Verification:</strong> Utilizes cryptographic signatures to authenticate the origin
+						of emails, enhancing security.
+					</li>
+					<li>
+						<strong class="text-green-500">ICP Platform:</strong> Leverages the low-cost infrastructure of the ICP platform for generating a
+						vast number of email addresses.
+					</li>
+					<li>
+						<strong class="text-green-500">Address Generation:</strong> There's a pressing need for generating a multitude of email
+						addresses assignable to customers for auditing purposes, without requiring customers to
+						forfeit their personal email addresses.
+					</li>
+					<li>
+						<strong class="text-green-500">ICP vetKD:</strong> Employs ICP vetKD to encrypt emails in transit and at rest, ensuring superior
+						security and privacy.
+					</li>
+				</ul>
+			</section>
+			<section id="architecture" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">Architecture</h2>
+				<p class="mb- 6 text-justify">DmailFi's architecture is built on several key components...</p>
+				<ul class="list-disc ml-5 mb- 6 text-justify">
+					<li>
+						<strong class="text-green-500">Master Canister:</strong> Serves as a registry linking domain names (e.g., user@example.com) to
+						Mail Canister IDs and facilitates the creation of canisters for users.
+					</li>
+					<li>
+						<strong class="text-green-500">Mail Canister:</strong> Functions as the storage unit for emails and files associated with a
+						domain name (e.g., example.com).
+					</li>
+					<li>
+						<strong class="text-green-500">Domain Verification Service (DVS):</strong> A Web2 API service confirming domain ownership,
+						instructing the Master Canister to generate a Mail Canister for the domain.
+					</li>
+					<li>
+						<strong class="text-green-500">Web3 SMTP Gateway:</strong> A gateway facilitating the reception of emails via the Simple Mail
+						Transfer Protocol from traditional email environments to the ICP network.
+					</li>
+					<li>
+                        <strong class="text-green-500">Frontend:</strong> Provides a workspace for domain owners to read and manage their emails.
+                    </li>
+				</ul>
+			</section>
+			<section id="registration" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">Registration Workflow</h2>
+				<p class="mb- 6 text-justify">DmailFi simplifies the registration process...</p>
+				<ul class="list-disc ml-5 mb- 6 text-justify">
+					<li>Users submit a domain name for verification via DVS.</li>
+					<li>
+						Post-verification, DVS prompts the Master Canister to create a corresponding Mail
+						Canister.
+					</li>
+					<li>Upon Mail Canister creation, users can commence sending emails to other domains.</li>
+				</ul>
+			</section>
+			<section id="mail" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">Mail Sending Workflow</h2>
+				<p class="mb- 6 text-justify">Email transmission in DmailFi operates seamlessly:</p>
+				<ul class="list-disc ml-5 mb- 6 text-justify">
+					<li>
+						Sending users' private Mail Canisters consult the Master Canister to retrieve the
+						recipient domain's Canister ID and direct emails accordingly.
+					</li>
+					<li>
+						For recipient domains not on the ICP platform, the Mail Canister employs an HTTP
+						Outgoing call to relay the email through the Web3 SMTP Gateway to external recipients.
+					</li>
+				</ul>
+			</section>
+			<section id="usecase" class="mb-12">
+				<h2 class="text-3xl font-bold mb-4 text-green-600">Use Cases</h2>
+				<p class="mb- 6 text-justify">
+					DmailFi revolutionizes email communication and financial transactions for:
+				</p>
+				<ul class="list-disc ml-5 mb- 6 text-justify">
+					<li>
+						Freelancers: Streamlining invoice issuance and payment reception directly via email.
+					</li>
+					<li>Businesses: Enhancing management of payments and communications.</li>
+					<li>
+						Regulated Industries: Health, Government and Legal Industries can send encrypted mails.
+					</li>
+					<li>
+						Individuals: Facilitating money transfers as effortlessly as sending an email, ideal for
+						bill splitting or gifting.
+					</li>
+				</ul>
+			</section>
+		</div>
+		<aside class="col-span-1">
+			<div class="sticky top-8">
+				<div class="mb-8">
+					<h3 class="text-xl font-semibold mb-4 text-green-500">Table of Contents</h3>
+					<ul class="space-y-2">
+						<li><a href="#abstract" class="text-green-600 hover:text-green-300">Abstract</a></li>
+						<li><a href="#intro" class="text-green-600 hover:text-green-300">Introduction</a></li>
+						<li><a href="#problem" class="text-green-600 hover:text-green-300">The Problem</a></li>
+						<li><a href="#solution" class="text-green-600 hover:text-green-300">The Solution</a></li>
+						<li>
+							<a href="#architecture" class="text-green-600 hover:text-green-300">Architecture</a>
+						</li>
+						<li>
+							<a href="#registration" class="text-green-600 hover:text-green-300"
+								>Registration Workflow</a
+							>
+						</li>
+						<li>
+							<a href="#mail" class="text-green-600 hover:text-green-300">Mail Sending Workflow</a>
+						</li>
+						<li><a href="#usecase" class="text-green-600 hover:text-green-300">Use Cases</a></li>
+					</ul>
+				</div>
+			</div>
+		</aside>
+	</main>
+</div>
+
+<style>
+	.active {
+		color: #000;
+	}
+</style>
